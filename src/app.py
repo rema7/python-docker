9a8c833fa3e4524388e21e7a3a12bdb2c97e1485
Maxim Cherkasov <maxim.cherkasov@gmail.com>
2017-09-22 18:08:16 +0300

    init

--- src/app.py
+++ src/app.py
@@ -3,7 +3,12 @@ import falcon
 from api.resources import (
     RestResource,
 )
+from middlewares import (
+    ContentEncodingMiddleware,
+)
 
-app = falcon.API()
+app = falcon.API(middleware=[
+    ContentEncodingMiddleware(),
+])
 
 app.add_route('/rest', RestResource())
